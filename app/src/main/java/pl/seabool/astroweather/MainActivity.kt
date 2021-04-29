3be977b96a2721ba4e9e8b3598ffcd2f6b400451
Sebastian Stachecki <xenophobiouspl@gmail.com>
2021-04-30 00:38:10 +0200

    chore(project): add ViewPager with Sun and Moon

--- app/src/main/java/pl/seabool/astroweather/MainActivity.kt
+++ app/src/main/java/pl/seabool/astroweather/MainActivity.kt
@@ -1,11 +1,50 @@
 package pl.seabool.astroweather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(SunFragment(), "Sun")
+        adapter.addFragment(MoonFragment(), "Moon")
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+    }
+
+    class ViewPagerAdapter(manager: FragmentManager) :
+        FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        private val fragmentList: MutableList<Fragment> = ArrayList()
+        private val titleList: MutableList<String> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList.add(fragment)
+            titleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
+
     }
 }
